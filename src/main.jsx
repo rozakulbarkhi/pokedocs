22f0f8f54e51e15420f914db98f68248419ab4ce
Rozakul Barkhi <rozakulbarkhirb@gmail.com>
2023-08-11 15:21:24 +0700

    init commit

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./layouts/Layout";
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Layout>
+        <Home />
+      </Layout>
+    ),
+  },
+  {
+    path: "/detail/:name",
+    element: (
+      <Layout>
+        <Detail />
+      </Layout>
+    ),
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
